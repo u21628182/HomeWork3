df5635aef88ab19f23cc8019628cde6f2875696a
rikus <rikus@RIKUS>
2023-10-29 20:50:53 +0200

    Add project files.

---
+++ HomeWork3/Controllers/HomeDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeWork3.Models;
+using HomeWork3.ViewModels;
+
+namespace HomeWork3.Controllers
+{
+    public class HomeDataController : Controller
+    {
+        // GET: HomeData
+        public ActionResult MultiData()
+        {
+            namesEntities obj = new pebsEntities();
+
+            var myModel
+            return View();
+        }
+    }
+}
---
+++ HomeWork3/ViewModels/StudentBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HomeWork3.Models;
+
+namespace HomeWork3.ViewModels
+{
+    public class StudentBook
+    {
+        public IEnumerable<students> studentsss { get; set; }
+        public IEnumerable<books> booksss { get; set; }
+    }
+}
